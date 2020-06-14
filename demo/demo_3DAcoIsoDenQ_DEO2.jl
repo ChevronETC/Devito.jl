cf1e16c2a2a26544ebaa4b3e97ba6356551c2a02
Sam Kaplan <Sam.Kaplan@chevron.com>
2020-06-14 18:54:14 +0000

    dial back the Q

--- demo/demo_3DAcoIsoDenQ_DEO2.jl
+++ demo/demo_3DAcoIsoDenQ_DEO2.jl
@@ -17,25 +17,25 @@ v = Devito.Function(name="v", grid=grid, space_order=8)
 q = Devito.Function(name="woverq", grid=grid, space_order=8)
 
 copy!(b, ones(Float32,size(grid))) # alternative: fill!(b, 1)
-copy!(q, ones(Float32,size(grid))) # alternative: fill!(q, 1)
+copy!(q, (1/1000)*ones(Float32,size(grid))) # alternative: fill!(q, 1)
 copy!(v, 1.5f0*ones(Float32,size(grid))) # alternative: fill!(v, 1.5)
 
-time_range = TimeAxis(start=0.0f0, stop=1000.0f0, step=1.0f0)
+time_range = TimeAxis(start=0.0f0, stop=750.0f0, step=1.0f0)
 
 p = TimeFunction(name="p", grid=grid, time_order=2, space_order=8)
 t,x,y,z = dimensions(p)
 
 src = RickerSource(name="src", grid=grid, f0=0.01f0, npoint=1, time_range=time_range,
-    coordinates=[605.0 605.0 605.0])
+    coordinates=[10.0 605.0 605.0])
+src_term = inject(src; field=PyObject(p).forward, expr=src*spacing(t)^2*v^2/b)
 
 nz,ny,nx,δz,δy,δx = size(grid)...,spacing(grid)...
-rec_coords = zeros(nz,3)
-rec_coords[:,1] .= 0.6*δx*(nx-1)
-rec_coords[:,2] .= 1 # TODO
-rec_coords[:,3] .= 1 # TODO
+rec_coords = zeros(nx,3)
+rec_coords[:,1] .= δx*[0:nx-1;]
+rec_coords[:,2] .= 605.0
+rec_coords[:,3] .= 20.0
 rec = Receiver(name="rec", grid=grid, npoint=nz, time_range=time_range, coordinates=rec_coords)
-
-src_term = inject(src; field=PyObject(p).forward, expr=src*#=spacing(t)^2*=#v^2/b)
+rec_term = interpolate(rec, expr=p)
 
 g1(field) = dx(field,x0=x+spacing(x)/2)
 g2(field) = dy(field,x0=y+spacing(y)/2)
@@ -55,7 +55,7 @@ dt = step(time_range)
 smap = spacing_map(grid)
 smap[spacing(t)] = dt
 
-op = Operator([stencil_p, src_term], subs=smap, name="OpExampleIso")
+op = Operator([stencil_p, src_term, rec_term], subs=smap, name="OpExampleIso")
 
 apply(op)
 
@@ -64,5 +64,10 @@ using PyPlot, PyCall
 _p = data(p)
 extrema(_p)
 
-imshow(_p[1,:,60,:])
+d = data(rec)
+
+figure();imshow(_p[1,:,60,:])
+display(gcf())
+
+figure();imshow(d, aspect="auto",cmap="gray",clim=.1*[-1,1]*maximum(abs,d))
 display(gcf())
