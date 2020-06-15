67e6210bb81dbacfa25f1120a8cdddc177fde8a4
Sam Kaplan <Sam.Kaplan@chevron.com>
2020-06-15 19:42:58 +0000

    make sure mpi stuff is installed

--- deps/build.jl
+++ deps/build.jl
@@ -3,4 +3,7 @@ using Conda
 Conda.add("pip")
 pip = joinpath(Conda.BINDIR, "pip")
 run(`$pip install git+https://github.com/devitocodes/devito.git`)
-run(`$pip install --upgrade sympy==1.5`)
+#run(`$pip install devito[extras]`)
+run(`$pip install mpi4py`)
+run(`$pip install ipyparallel`)
+run(`$pip install --upgrade sympy<1.6`)
