7e8a56cb18671f7f4b90436a96c463ff0426da93
Sam Kaplan <Sam.Kaplan@chevron.com>
2020-06-18 15:59:10 +0000

    reorg

--- deps/build.jl
+++ deps/build.jl
@@ -3,7 +3,9 @@ using Conda
 Conda.add("pip")
 pip = joinpath(Conda.BINDIR, "pip")
 run(`$pip install git+https://github.com/devitocodes/devito.git`)
-#run(`$pip install devito[extras]`)
 run(`$pip install mpi4py`)
 run(`$pip install ipyparallel`)
 run(`$pip install --upgrade sympy<1.6`)
+
+#run(`$pip install devito`)
+#run(`$pip install devito[extras]`)
