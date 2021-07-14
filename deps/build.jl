756c48c210cad495cabfa271be84199886e5298a
John Washbourne <jwashbourne@chevron.com>
2021-07-14 18:29:22 +0000

    update built for cython and versioneer

--- deps/build.jl
+++ deps/build.jl
@@ -2,11 +2,11 @@ using Conda
 
 Conda.add("pip")
 pip = joinpath(Conda.BINDIR, "pip")
+run(`$pip install cython`) 
+run(`$pip install versioneer`) 
 run(`$pip install git+https://github.com/devitocodes/devito.git`)
 run(`$pip install mpi4py`)
 run(`$pip install ipyparallel`)
-run(`$pip install --upgrade sympy'<'1.6`)
-run(`$pip install numpy`)
 
 #run(`$pip install devito`)
 #run(`$pip install devito[extras]`)
