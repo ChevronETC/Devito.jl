09a0b4610606c7074e76bcbdfaf5818d602dcda8
deckerla@gmail.com <lukedecker@chevron.com>
2022-03-23 21:41:27 +0000

    Revert "registryci fix"
    This reverts commit 98d916444e3b02c50583a547b90a2ae40001d854.

--- deps/build.jl
+++ deps/build.jl
@@ -1,20 +1,12 @@
 using Conda
 
-try
-    Conda.add("pip")
-    pip = joinpath(Conda.BINDIR, "pip")
-    run(`$pip install cython`) 
-    run(`$pip install versioneer`) 
-    run(`$pip install git+https://github.com/devitocodes/devito.git`)
-    run(`$pip install mpi4py`)
-    run(`$pip install ipyparallel`)
-catch e
-    if get(ENV, "JULIA_REGISTRYCI_AUTOMERGE", "false") == "true"
-        @warn unable to build
-    else
-        throw(e)
-    end
-end
+Conda.add("pip")
+pip = joinpath(Conda.BINDIR, "pip")
+run(`$pip install cython`) 
+run(`$pip install versioneer`) 
+run(`$pip install git+https://github.com/devitocodes/devito.git`)
+run(`$pip install mpi4py`)
+run(`$pip install ipyparallel`)
 
 #run(`$pip install devito`)
 #run(`$pip install devito[extras]`)
