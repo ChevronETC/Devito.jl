c93dd983723711a468b36b97be84475d686cd969
lukedecker <lukedecker@chevron.com>
2022-12-14 21:26:03 +0000

    adding devitopro option

--- deps/build.jl
+++ deps/build.jl
@@ -15,5 +15,21 @@ catch e
         throw(e)
     end
 end
+# set the devito pro repo to DEVITO_PRO environmental variable
+dpro_repo = get(ENV, "DEVITO_PRO", "")
+if dpro_repo != ""
+    @info "Building DevitoPro"
+    try 
+        pip = joinpath(Conda.BINDIR, "pip")
+        run(`$pip install git+$(dpro_repo)`)
+    catch e
+        if get(ENV, "JULIA_REGISTRYCI_AUTOMERGE", "false") == "true"
+            @warn unable to build
+        else
+            throw(e)
+        end
+    end
+end
+
 #run(`$pip install devito`)
 #run(`$pip install devito[extras]`)
