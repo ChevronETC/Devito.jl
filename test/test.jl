6bca2fad452ac25d49d76f7c0680204513701ab8
lukedecker <lukedecker@chevron.com>
2023-04-03 22:00:39 +0000

    wip

--- test/test.jl
+++ test/test.jl
@@ -5,3 +5,12 @@ run(`python test.py`)
 
 @info "pyinclude"
 @pyinclude("test.py")
+
+
+using Devito
+@info "using Devito"
+@info "run"
+run(`python test.py`)
+
+@info "pyinclude"
+@pyinclude("test.py")
