b0e70263e4cb47ae66bc5bdbbc38581c838a9b8d
lukedecker <lukedecker@chevron.com>
2023-04-04 18:46:35 +0000

    workaround

--- test/test.jl
+++ test/test.jl
@@ -1,16 +1,33 @@
-using PyCall
+# using PyCall
 
-@info "run"
-run(`python test.py`)
+# @info "run"
+# run(`python test.py`)
 
-@info "pyinclude"
-@pyinclude("test.py")
+# @info "pyinclude"
+# @pyinclude("test.py")
 
+# @info "using Devito"
+using Libdl
+Libdl.dlopen("libgomp.so.1", Libdl.RTLD_GLOBAL)
+using Devito, MPI
+MPI.Init()
+# Libdl.dlopen("libnvc.so", Libdl.RTLD_LAZY | Libdl.RTLD_GLOBAL)
+# Libdl.dlopen("libnvcpumath.so", Libdl.RTLD_LAZY | Libdl.RTLD_GLOBAL)
+# Libdl.dlopen("libssp.so.0", Libdl.RTLD_LAZY | Libdl.RTLD_GLOBAL)
+# @info "$(Libdl.dllist())"
+# @info "Julia approach"
+g = Grid(shape=(60,70,80))
+u = TimeFunction(name="u", grid=g)
+eq = Eq(forward(u), u+1)
+op = Operator([eq], name="ezpz")
+apply(op,time_m=0, time_M=500)
+myarray = convert(Array, data(u))
+if MPI.Comm_rank(MPI.COMM_WORLD) == 0
+    @show extrema(myarray)
+end
 
-using Devito
-@info "using Devito"
-@info "run"
-run(`python test.py`)
+# @info "run"
+# run(`python test.py`)
 
-@info "pyinclude"
-@pyinclude("test.py")
+# @info "pyinclude"
+# @pyinclude("test.py")
--- test/test.py
+++ test/test.py
@@ -1,15 +1,15 @@
-import devito
+import devito, devitopro
 
 def ezpz(shape,time_M):
     g = devito.Grid(shape=shape)
     u = devito.TimeFunction(grid=g, name="u")
-    eq = devito.Eq(u.forward,u+1)
+    eq = devito.Eq(u.forward,u+2)
     op = devito.Operator([eq], name="ezpz")
     op.apply(time_m=0, time_M=time_M)
     print(u.data.min(), u.data.max())
 
 
-shape = (60,70,80)
-time_M = 600
+shape = (4,4,4)
+time_M = 3
 
 ezpz(shape, time_M)
