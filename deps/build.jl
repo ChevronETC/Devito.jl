3bd5930edc85cf4f2cc5362228b6bad18756a9a3
lukedecker <lukedecker@chevron.com>
2023-10-17 19:11:19 +0000

    default build to latest released devito version

--- deps/build.jl
+++ deps/build.jl
@@ -1,11 +1,16 @@
 using Conda
 
 dpro_repo = get(ENV, "DEVITO_PRO", "")
-which_devito = get(ENV,"DEVITO_BRANCH", "master")
+which_devito = get(ENV,"DEVITO_BRANCH", "")
 try
     Conda.pip_interop(true)
-    @info "Building devito from branch $(which_devito)"
-    Conda.pip("install", "devito[tests,extras,mpi]@git+https://github.com/devitocodes/devito@$(which_devito)")
+    if which_devito != ""
+        @info "Building devito from branch $(which_devito)"
+        Conda.pip("install", "devito[tests,extras,mpi]@git+https://github.com/devitocodes/devito@$(which_devito)")
+    else
+        @info "Building devito from latest release"
+        Conda.pip("install", "devito[tests,extras,mpi]@git+https://github.com/devitocodes/devito")
+    end
     # optional devito pro installation
     if dpro_repo != ""
         Conda.pip("install","git+$(dpro_repo)")
