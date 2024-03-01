85720a23bdbfaccdf897f5f2bd687b036e0f9803
samkaplan <sam.kaplan@chevron.com>
2024-03-01 12:58:41 +0000

    get CI working

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,11 @@
-using Devito
+using Devito, MPI
+
 for testscript in ("serialtests.jl", "gencodetests.jl", "csymbolicstests.jl")
     include(testscript)
 end
 
-run(`mpirun -n 2 julia --code-coverage mpitests_2ranks.jl`)
-run(`mpirun -n 4 julia --code-coverage mpitests_4ranks.jl`)
+run(`$(mpiexec()) -n 2 julia --code-coverage mpitests_2ranks.jl`)
+run(`$(mpiexec()) -n 4 julia --code-coverage mpitests_4ranks.jl`)
 
 if Devito.has_devitopro()
     include("devitoprotests.jl")
