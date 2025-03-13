927608bdb5f020a2e42132ec810ddf1ba086bbe4
jwashbourne <jwashbourne@chevron.com>
2025-03-13 19:16:14 +0000

    change  to mpirun

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ for testscript in ("serialtests.jl", "gencodetests.jl", "csymbolicstests.jl")
     include(testscript)
 end
 
-run(`$(mpiexec()) -n 2 julia --code-coverage mpitests_2ranks.jl`)
+run(`mpirun -n 2 julia --code-coverage mpitests_2ranks.jl`)
 
 if Devito.has_devitopro()
     @info "running devito pro tests"
