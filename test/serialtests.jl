0547f126b018aec64510ae61475f3c86d44dc2a6
miladbader <milad.bader@chevron.com>
2026-02-06 15:36:27 +0000

    add @test_broken to sympy_or

--- test/serialtests.jl
+++ test/serialtests.jl
@@ -521,7 +521,7 @@ end
     for j in 1:div(size,factr)+1
         @test data(f)[j] == data(g)[(j-1)*factr+1]
     end
-    # @test data(f)[end] == data(g)[end]
+    @test_broken data(f)[end] == data(g)[end]
 end
 
 @testset "PyObject(Dimension)" begin
